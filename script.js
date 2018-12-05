9fdb752f7118f8419862a88a4462bf03825e4b06
joelpolanco007 <43018327+joelpolanco007@users.noreply.github.com>
2018-12-05 14:59:43 -0800

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$("#icecream").click(function(){
+ $("#icecreamimage").slideToggle();   
+});
+
+$("#jellyfishstank").click(function(){
+ $("#jellyfish").fadeOut();   
+});
+
+$("#clarinetstank").click(function(){
+ $("#clarinetsucks").fadeToggle();   
+});
